e49500a1af6e5d1c107bc8fddf17fd18f9a084c9
PedrovSilva <82241565+PedrovSilva@users.noreply.github.com>
2024-05-03 13:51:03 -0400

    feat: add post thread route

--- routes/threadsRoutes.js
+++ routes/threadsRoutes.js
@@ -5,20 +5,51 @@ const router = express.Router();
 const { Thread } = require("../models/threadModel");
 const Board = require("../models/boardModel");
 
-router.route("/api/threads/:board").post(async (req, res) => {
-  const board = await Board.findOne({ boardName: req.params.board });
+router
+  .route("/api/threads/:board")
+  .post(async (req, res) => {
+    const board = await Board.findOne({ boardName: req.params.board });
 
-  try {
-    if (!board) {
-      const newBoard = new Board({ boardName: req.params.board });
-      const board = await newBoard.save();
+    try {
+      if (!board) {
+        const newBoard = new Board({ boardName: req.params.board });
+        const board = await newBoard.save();
+      }
+      const newThread = new Thread({ text: req.body.text });
+      newThread.setPassword(req.body.delete_password);
+
+      board.threads.push(newThread);
+      await board.save();
+
+      res.json(board);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Server error" });
     }
-    const newThread = new Thread({ text: req.body.text });
-    newThread.setPassword(req.body.delete_password);
+  })
+  .get(async (req, res) => {
+    try {
+      const board = await Board.findOne({
+        boardName: req.params.board,
+      }).populate({
+        path: "threads",
+        options: { sort: { bumped_on: -1 }, limit: 10 },
+        select: "-reported -delete_password",
+        populate: {
+          path: "replies",
+          options: { sort: { created_on: -1 }, limit: 3 },
+          select: "-reported -delete_password",
+        },
+      });
+      if (!board) {
+        return res.status(404).json({ error: "Board not found" });
+      }
 
-    board.threads.push(newThread)
-    await board.save()
-  } catch (err) {
-    console.error(err);
-  }
-}).get()
+      res.json(board.threads);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Server error" });
+    }
+  });
+
+module.exports = router;
