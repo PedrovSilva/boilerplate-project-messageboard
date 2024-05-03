4180cbbdf3f9f9c291eeaf1fb50bcaffd45c2e2c
PedrovSilva <82241565+PedrovSilva@users.noreply.github.com>
2024-05-03 13:24:56 -0400

    feat: add post thread routes

--- routes/threadsRoutes.js
+++ routes/threadsRoutes.js
@@ -1,16 +1,23 @@
-'use strict';
+"use strict";
 
-const express = require("express")
-const router = express.Router()
-const {Thread} = require("../models/threadModel")
-const Board = require("../models/boardModel")
+const express = require("express");
+const router = express.Router();
+const { Thread } = require("../models/threadModel");
+const Board = require("../models/boardModel");
 
-router.route("/api/threads/:board"). post(async(req, res) => {
-    const board = await Board.findOne({boardName: req.params.board})
+router.route("/api/threads/:board").post(async (req, res) => {
+  const board = await Board.findOne({ boardName: req.params.board });
 
-    if(!board){
-        const newBoard = {boardName: req.params.board}
-        const board = await newBoard.save()
+  try {
+    if (!board) {
+      const newBoard = new Board({ boardName: req.params.board });
+      const board = await newBoard.save();
     }
-    
-})
+    const newThread = new Thread({ text: req.body.text });
+    newThread.setPassword(req.body.delete_password);
+
+    await newThread.save();
+  } catch (err) {
+    console.error(err);
+  }
+});
